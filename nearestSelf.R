619337c848442df0dad8deddf6c57234d31be1ed
Gareth Robins <gareth.robins@gmail.com>
2015-07-14 00:03:10 +1200

    solved weighting

--- nearestSelf.R
+++ nearestSelf.R
@@ -5,14 +5,14 @@ imp <- read.table('test_points.csv', sep=',', header=T)
 imp$w <- 1
 
 
-nearestSelf <- function(df, gr = NULL){
+nearestSelf <- function(df, gr = NULL, n){
   require(FNN)
   if(nrow(gr)>0){
-    df[get.knnx(df, gr, 2)$nn.index[, 2],]
-    
+    df[get.knnx(df, gr, n)$nn.index[, n],]
+  } else {
+    df[get.knnx(df, df, n)$nn.index[, n],]
   }
-  df[get.knnx(df, df, 2)$nn.index[, 2],]
-}
+}  
 
 
 gravityWeight <- function(p1, p2){
@@ -34,18 +34,36 @@ gravity <- function(df, n){
   
   for (i in 1:n){
     
-    NN <- nearestSelf(df, gr)
-    NN$w <- 1
-    df <- gravityWeight(df, NN)
+    if (i==1){
+      
+      NN <- nearestSelf(df, df, 2)
+      NN$w <- 1
+      gr <- gravityWeight(df, NN)
+      
+    } else {
+      
+      
+      NN <- nearestSelf(df, gr, 2)
+      NN$w <- gr[, 3]
+      gr <- gravityWeight(df, NN)
+      
+      
+    }
     
+
   }
-  df
+  print(gr[1,])
+  gr
 }
 
-mid <- gravity(imp, 2000)
-par(mfrow=c(2, 2))
+
+
+mid <- gravity(imp, 1000)
 plot(imp[, 1:2], pch=1)
 points(mid[, 1:2], col='blue', pch=19)
+points(nearestSelf(mid, mid, 2)[, 1:2], col='green')
+
+par(mfrow=c(2, 2))
 par(mfrow=c(1, 1))
 
 
